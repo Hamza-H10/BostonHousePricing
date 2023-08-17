5c463642f2daadd8b31bf028c9373a845f729c8f
Hamza Haider <123259911+Hamza-H10@users.noreply.github.com>
2023-08-17 14:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-# import pickle
-# from flask import Flask, request, app, jsonify, url_for, render_template
-
-# import numpy as np
-# import pandas as pd
-
-# app = Flask(__name__)  # this is starting point from where the app will run
-
-# # Load the model
-# regmodel = pickle.load(open("regmodel.pkl", "rb"))
-
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
-
-
-# @app.route('/predict_api', methods=['POST'])
-# def predict_api():
-#     # whenever i hit the /predict_api as a post request then input that we will give is going to be in the JSON format which will be captured inside the "data" key and its get stored inside the data variable
-#     data = request.json['data']
-#     print(data)
-# # once we get the data it will be in a key value pair
-#     print(np.array(list(data.values())).reshape(1, -1))
-#     new_data = scalar.trasform(np.array(list(data.values())).reshape(1, -1))
-#     output = regmodel.predict(new_data)
-#     print(output[0])
-#     return jsonify(output[0])
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 import json
 import pickle
 
